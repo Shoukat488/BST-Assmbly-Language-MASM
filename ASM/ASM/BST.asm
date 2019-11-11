291e5eaaa4801c842e7983b8de932c0b691efaa4
Shoukat Ali <shoukatali488@gmail.com>
2019-11-11 20:47:44 +0500

    Tried to implement tree in it

--- ASM/ASM/BST.asm
+++ ASM/ASM/BST.asm
@@ -1,29 +1,16 @@
 include Irvine32.inc
+TreeNode struc
+  data dword ?
+  leftNode TreeNode <>
+  rightNode TreeNode <>
+  
 
-Get_frequencies proto , alphaTab : ptr byte, freqTab : dword
+TreeNode ends
 .data
-target BYTE "AAEBDCFBBC",0
-freqTable DWORD 256 DUP(0)
+
+
 .code
 main proc
 
-INVOKE Get_frequencies, ADDR target, ADDR freqTable
-exit
-main endp
-Get_frequencies proc , alphaTab : ptr byte, freqTab : dword
 
-mov esi, alphaTab
-
-mov ecx , lengthof alphaTab
-
-l1:
-
-mov al , [esi]
-mov eax , al
-add freqTab[eax] , 1
-inc esi
-loop l1
-
-ret
-Get_frequencies endp
 end main
