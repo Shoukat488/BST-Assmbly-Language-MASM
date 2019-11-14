64dc8e732fb813e91e674ef80f0b07a2cb9e977f
Shoukat Ali <shoukatali488@gmail.com>
2019-11-14 23:53:18 +0500

    Added traversal function

--- ASM/ASM/BST.asm
+++ ASM/ASM/BST.asm
@@ -1,9 +1,11 @@
 INCLUDE Irvine32.inc 
 insert proto  , left : dword , right: dword 
+traverseInOrder proto , rootNode : ptr byte
 .data
 bst dword 1000 dup(1)
-array dword 4,5,6,8,9
+array dword 5,4,8,6,7,10,2,18,14,15
 multi dword 4
+dividend dword 4
 tempIndex dword ?
 .code
 main PROC
@@ -22,21 +24,21 @@ mov ebx, array[edx]
 mov tempIndex , edx
 
 invoke insert  , addr [ bst + 4 ] , addr [ bst + 8 ] 
-
+mov edi , offset bst
 mov edx , tempIndex
 add edx , 4
 
 loop l1
 
-mov ecx , 70
-mov esi , 0
-
-l2:
-mov eax , bst[esi]
-call writeDec
 call crlf
-add esi , 4
-loop l2
+
+mov edx , 0
+mov eax , 0
+mov ebx , 0
+mov ecx , 0
+
+mov esi , offset bst
+invoke traverseInOrder , addr bst
 
 exit
 
@@ -56,13 +58,17 @@ jb leftNode
 jmp rightNode
 
 leftNode:
-cmp [left] , 1
+mov eax, [left]
+mov eax , [eax]
+cmp eax , 1
 je assignValueAtLeft
 
 mov eax , left
 mov edi , eax
 sub eax , esi
 mul multi
+mov edx , 0
+div dividend
 add left , eax 
 add left , 4
 add right , eax
@@ -73,15 +79,22 @@ jmp final
 
 assignValueAtLeft:
 mov [left] , ebx
+mov eax , [left]
+call writeDec
+call crlf
 jmp final
 
 rightNode:
-cmp [right] ,1
+mov eax, [right]
+mov eax , [eax]
+cmp eax ,1
 je assignValueAtRight
 mov eax , right
 mov edi , eax
 sub eax , esi
 mul multi
+mov edx , 0
+div dividend
 add left , eax 
 add left , 4
 add right , eax
@@ -91,11 +104,17 @@ invoke insert , left , right
 jmp final
 
 assignValueAtRight:
-mov [edi] , ebx
+mov [right] , ebx
+mov eax , [right]
+call writeDec
+call crlf
 jmp final
 
 assigValueAtRoot:
 mov [edi] , ebx
+mov eax , [edi]
+call writeDec
+call crlf
 jmp final
 
 final:
@@ -103,10 +122,50 @@ ret
 
 insert endp
 
-traverseInOrder proc
+traverseInOrder proc , rootNode : ptr byte
 
+mov eax , rootNode
+mov ebx , rootNode
+sub eax , esi
+mul multi
+mov edx , 0
+div dividend
+add ebx , eax
+add ebx , 4
+mov eax , [ebx]
 
+cmp eax , 1
+jne traverseLeftNode
 
+print:
+mov eax , [rootNode]
+mov eax , [eax]
+call writeDec
+call crlf
 
+mov eax , rootNode
+mov ebx , rootNode
+sub eax , esi
+mul multi
+mov edx , 0
+div dividend
+add ebx , eax
+add ebx , 8
+mov eax , [ebx]
+
+cmp eax , 1
+jne traverseRightNode
+jmp final
+
+traverseRightNode:
+invoke traverseInOrder , ebx
+jmp final
+
+traverseLeftNode:
+invoke traverseInOrder , ebx
+jmp print
+
+final:
+ret
 traverseInOrder endp
 end main
