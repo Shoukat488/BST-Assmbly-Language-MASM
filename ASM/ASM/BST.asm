2fa823337c003962a7478ecc18562a8980424c0a
Shoukat Ali <shoukatali488@gmail.com>
2019-11-15 19:03:07 +0500

    Insertion and traversal functions are fixed and woking fine

--- ASM/ASM/BST.asm
+++ ASM/ASM/BST.asm
@@ -1,35 +1,65 @@
 INCLUDE Irvine32.inc 
-insert proto  , left : dword , right: dword 
 traverseInOrder proto , rootNode : ptr byte
+searchInTree proto , rootNode : ptr byte , value : dword
 .data
-bst dword 1000 dup(1)
-array dword 5,4,8,6,7,10,2,18,14,15
+array dword 5,4,8,6,7,10,2,3,11,14,13,12,26
 multi dword 4
 dividend dword 4
 tempIndex dword ?
+p1 byte "Inserting values : ",0
+p2 byte "Traversing binray Tree in order",0
+p3 byte "Enter value to search in tree : ",0
+foundString byte "Value found in tree ",0
+nFoundString byte "Value not found in tree ",0
+bst dword 1000 dup(1)
 .code
 main PROC
-
-
-mov edx , 0
+;-------------------------
+mov edx , offset p1
+call writeString
+call crlf
+mov edx , 4
 mov eax , 0
 mov ebx , 0
 mov ecx , 0
-
-mov esi , offset bst
+mov [bst] , 5
 mov ecx, lengthof array
 mov edi , offset bst
+
+
 l1:
+
 mov ebx, array[edx]
 mov tempIndex , edx
 
-invoke insert  , addr [ bst + 4 ] , addr [ bst + 8 ] 
+push offset [bst + 4]
+push offset [bst + 8]
+
+call insert
+
+add esp , 8
 mov edi , offset bst
 mov edx , tempIndex
 add edx , 4
 
 loop l1
 
+call crlf
+mov ecx, 15
+mov esi, 0
+
+l4:
+
+mov eax , bst[esi]
+call writeDec
+call crlf
+add esi , 4
+loop l4
+;-----------------------------------
+call crlf
+
+mov edx , offset p2
+call writeString
 call crlf
 
 mov edx , 0
@@ -40,72 +70,112 @@ mov ecx , 0
 mov esi , offset bst
 invoke traverseInOrder , addr bst
 
+
+;----------------------------------------------
+
+mov edx , offset p3
+call writestring
+call crlf
+call readInt
+mov edx , 0
+mov ebx , 0
+mov ecx , 0
+
+invoke searchInTree , offset bst , eax
+
+cmp ecx , 1
+je found
+jmp notFound
+
+found:
+mov edx , offset foundString
+call writeString
+call crlf
+jmp final
+
+notFound:
+mov edx , offset nFoundString
+call writeString
+call crlf
+jmp final
+
+final:
 exit
 
 
 main endp
-insert proc , left : dword , right: dword 
+;//////////////////////////////////////////////// Insert Function //////////////////////////////////////////
+insert proc
+
+push ebp
+mov ebp , esp
 
 cmp ebx , [edi]
 je final
 
 mov eax , [edi]
-cmp eax , 1
-je assigValueAtRoot
 
 cmp  ebx , eax
 jb leftNode
 jmp rightNode
 
 leftNode:
-mov eax, [left]
+mov eax, [ebp + 12]
 mov eax , [eax]
 cmp eax , 1
 je assignValueAtLeft
 
-mov eax , left
+mov eax , [ebp + 12]
 mov edi , eax
-sub eax , esi
+sub eax , offset bst
 mul multi
 mov edx , 0
 div dividend
-add left , eax 
-add left , 4
-add right , eax
-add right , 8
+mov esi , [ebp + 12]
+add esi , eax 
 
-invoke insert , left , right 
+add esi , 4
+push esi
+add esi , 4
+push esi
+
+call insert 
 jmp final
 
 assignValueAtLeft:
-mov [left] , ebx
-mov eax , [left]
+mov esi , [ebp + 12]
+mov [esi] , ebx
+mov eax , [esi]
 call writeDec
 call crlf
 jmp final
 
 rightNode:
-mov eax, [right]
+mov eax, [ebp + 8]
 mov eax , [eax]
 cmp eax ,1
 je assignValueAtRight
-mov eax , right
+mov eax , [ebp + 8]
 mov edi , eax
-sub eax , esi
+sub eax , offset bst
 mul multi
 mov edx , 0
 div dividend
-add left , eax 
-add left , 4
-add right , eax
-add right , 8
+mov esi , [ebp + 8]
+add esi , eax 
 
-invoke insert , left , right
+add esi , 4
+push esi
+add esi , 4
+push esi
+
+call insert
 jmp final
 
 assignValueAtRight:
-mov [right] , ebx
-mov eax , [right]
+mov esi , [ebp + 8]
+mov [esi] , ebx
+mov eax , [esi]
 call writeDec
 call crlf
 jmp final
@@ -118,10 +188,14 @@ call crlf
 jmp final
 
 final:
-ret
+
+mov esp , ebp
+pop ebp 
+
+ret 8
 
 insert endp
-
+;///////////////////////////////////////////////// Traverse Function ///////////////////////////////////////
 traverseInOrder proc , rootNode : ptr byte
 
 mov eax , rootNode
@@ -168,4 +242,9 @@ jmp print
 final:
 ret
 traverseInOrder endp
+;////////////////////////////////////////// Search Function ////////////////////////////////////////////
+
+searchInTree proc , rootNode : ptr byte , value : dword
+
+searchInTree endp
 end main
